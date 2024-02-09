cfececc0c519a22829b9a0ee2972ebc400a316ef
Mehul-Natu <natu.m@northeastern.edu>
2024-02-09 18:25:59 -0500

    Added test files

---
+++ unitTest/Dummytest.test.js
@@ -0,0 +1,13 @@
+test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3)
+  });
+  
+  // Test case 2: Subtraction
+  test('subtracts 2 - 1 to equal 1', () => {
+    expect(2 - 1).toBe(1)
+  });
+  
+  // Test case 3: Multiplication
+  test('multiplies 3 * 4 to equal 12', () => {
+    expect(3 * 4).toBe(12);
+  });
