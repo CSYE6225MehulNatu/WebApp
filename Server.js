e3b59fa55ee35012e2ca8d061a3504b5b2f4c605
Mehul-Natu <natu.m@northeastern.edu>
2024-02-14 17:25:00 -0500

    added supertest in package json and added more integration test

--- Server.js
+++ Server.js
@@ -11,7 +11,12 @@ app.use("/v1/user/", userRouter)
 
 const port = process.env.PORT || 3001;
 
-app.listen(port, () => {
+
+const server = app.listen(port, () => {
     sync()
     console.log("Listening on port  3001....")
 }); 
+
+
+
+module.exports = {app, server};
