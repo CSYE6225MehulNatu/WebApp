124331eb3303ec534ecaee6abb72a654eeaffc30
Mehul-Natu <natu.m@northeastern.edu>
2024-02-24 02:01:54 -0500

    changes in runner and installation script

--- packer/runner.sh
+++ packer/runner.sh
@@ -12,17 +12,5 @@ sudo systemctl enable WebappService
 sudo systemctl start WebappService
 sudo systemctl status WebappService
 
-sudo systemctl start mysqld
-sudo systemctl enable mysqld
-
-echo "Checking if '$DB_DATABASE' database exists..."
-DB_EXISTS=$(mysql -u root -e "SHOW DATABASES LIKE '$DB_DATABASE';" | grep "$DB_DATABASE" > /dev/null; echo "$?")
-if [ "$DB_EXISTS" -eq 1 ]; then
-    echo "Database does not exist. Creating now..."
-    mysql -u root -e "CREATE DATABASE $DB_DATABASE;"
-    echo "Database '$DB_DATABASE' created."
-else
-    echo "Database '$DB_DATABASE' already exists."
-fi
 
 
