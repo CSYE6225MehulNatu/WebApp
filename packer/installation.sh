3cb59b698317f8f17dd4d53c6cc4008a410e7906
Mehul-Natu <natu.m@northeastern.edu>
2024-02-24 02:01:54 -0500

    changes in runner and installation script

--- packer/installation.sh
+++ packer/installation.sh
@@ -57,4 +57,18 @@ fi
 echo "Installing MySQL server"
 sudo dnf install -y mysql-server
 
+sudo systemctl start mysqld
+sudo systemctl enable mysqld
+
+echo "Checking if '$DB_DATABASE' database exists..."
+DB_EXISTS=$(mysql -u root -e "SHOW DATABASES LIKE '$DB_DATABASE';" | grep "$DB_DATABASE" > /dev/null; echo "$?")
+if [ "$DB_EXISTS" -eq 1 ]; then
+    echo "Database does not exist. Creating now..."
+    mysql -u root -e "CREATE DATABASE $DB_DATABASE;"
+    echo "Database '$DB_DATABASE' created."
+else
+    echo "Database '$DB_DATABASE' already exists."
+fi
+
+
 
