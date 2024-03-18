f30027c6cf7f16ea5fd946fbea26cd8928a0afb1
Mehul-Natu <natu.m@northeastern.edu>
2024-03-18 19:26:05 -0400

    changes yml for ops agent in installation.sh

--- packer/installation.sh
+++ packer/installation.sh
@@ -42,3 +42,24 @@ sudo npm install mysql2
 
 sudo curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+
+
+sudo tee /etc/google-cloud-ops-agent/config.yaml > /dev/null <<EOF
+logging:
+  receivers:
+    my-app-receiver:
+      type: files
+      include_paths:
+        - /var/log/webapp.log
+      record_log_file_path: true
+  processors:
+    my-app-processor:
+      type: parse_json
+      time_key: time
+      time_format: "%Y-%m-%dT%H:%M:%S.%L%Z"
+  service:
+    pipelines:
+      default_pipeline:
+        receivers: [my-app-receiver]
+        processors: [my-app-processor]
+EOF
