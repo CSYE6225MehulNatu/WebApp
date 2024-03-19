18874bef5ee8eee273a40eb09b557e336d09d5f3
Mehul-Natu <natu.m@northeastern.edu>
2024-03-19 01:58:00 -0400

    added ops config using packer file provisioner

--- packer/installation.sh
+++ packer/installation.sh
@@ -44,22 +44,6 @@ sudo curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
 
-sudo tee /etc/google-cloud-ops-agent/config.yaml > /dev/null <<EOF
-logging:
-  receivers:
-    my-app-receiver:
-      type: files
-      include_paths:
-        - /var/log/webapp.log
-      record_log_file_path: true
-  processors:
-    my-app-processor:
-      type: parse_json
-      time_key: time
-      time_format: "%Y-%m-%dT%H:%M:%S.%L%Z"
-  service:
-    pipelines:
-      default_pipeline:
-        receivers: [my-app-receiver]
-        processors: [my-app-processor]
-EOF
+sudo rm /etc/google-cloud-ops-agent/config.yaml
+
+sudo mv /tmp/OpsAgentConfig.yaml /etc/google-cloud-ops-agent/config.yaml
