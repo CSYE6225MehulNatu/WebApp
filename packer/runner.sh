1f9717ed2388fe4e3a88212bda942d0b67d3f0f7
Mehul-Natu <natu.m@northeastern.edu>
2024-03-19 00:58:19 -0400

    log file permissions given to csye user and group

--- packer/runner.sh
+++ packer/runner.sh
@@ -2,6 +2,10 @@
 sudo chown -R csye6225:csye6225 /opt/csye6225/webapp/
 sudo chmod -R 750 /opt/csye6225/webapp/
 
+sudo touch /var/log/webapp.log
+sudo chown -R csye6225:csye6225 /var/log/webapp.log
+sudo chmod -R 750 /var/log/webapp.log
+
 echo "Moving webAppService to systemd path"
 sudo cp /tmp/WebappService.service /etc/systemd/system/WebappService.service
 
