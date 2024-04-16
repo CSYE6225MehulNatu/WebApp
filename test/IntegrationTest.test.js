49009320185f8c1a26de4b47fb768652c2a62e3b
Mehul-Natu <113635083+Mehul-Natu@users.noreply.github.com>
2024-04-16 19:15:20 -0400

    Update IntegrationTest.test.js

--- test/IntegrationTest.test.js
+++ test/IntegrationTest.test.js
@@ -35,11 +35,11 @@ afterAll(async () => {
 
 describe("Integration test for User APIs", () => {
 
-  describe("POST /v3/user", () => {
+  describe("POST /v8/user", () => {
     test("saving a user response should be status 200", async() => {
       //await sync();
       const response = await request(app)
-        .post("/v3/user")
+        .post("/v8/user")
         .send({
           first_name: "Jane",
           last_name: "Doe",
@@ -52,12 +52,12 @@ describe("Integration test for User APIs", () => {
     });
   });
 
-  describe("Get v3/user/self", () => {
+  describe("Get v8/user/self", () => {
     test("fetching a user response should be status 200", async() => {
       const token = "TE9MQGdtYWlsLmNvbTpsb2w=";
 
       const response = await request(app)
-      .get("/v3/user/self")
+      .get("/v8/user/self")
       .set("Authorization", "Basic " + token);
       
       expect(response.statusCode).toBe(200);
@@ -71,12 +71,12 @@ describe("Integration test for User APIs", () => {
     });
   });
 
-  describe("update v3/user/self", () => {
+  describe("update v8/user/self", () => {
     test("Updating a user, response should be status 204", async() => {
       const token = "TE9MQGdtYWlsLmNvbTpsb2w=";
 
       const response = await request(app)
-      .put("/v3/user/self")
+      .put("/v8/user/self")
       .set("Authorization", "Basic " + token)
       .send({
         first_name: "Peter",
@@ -88,11 +88,11 @@ describe("Integration test for User APIs", () => {
     });
   });
 
-  describe("GET updated v3/user/self", () => {
+  describe("GET updated v8/user/self", () => {
     test("fetching a user response should be status 200", async() => {
       const token = "TE9MQGdtYWlsLmNvbTpsb2xs";
       const response = await request(app)
-      .get("/v3/user/self")
+      .get("/v8/user/self")
       .set("Authorization", "Basic " + token);
       
       expect(response.statusCode).toBe(200);
